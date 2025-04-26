a7872cfc48cb79b52efae4a367c2c8f6947fabfb
Eric Drewitz <101157849+edrewitz@users.noreply.github.com>
2025-04-25 20:49:29 -0700

    Update cmaps.py

--- pyclimo/cmaps.py
+++ pyclimo/cmaps.py
@@ -22,4 +22,33 @@ def relative_humidity_colormap():
     
     return relative_humidity_colormap
 
+def temperature_change_colormap():
+    temperature_change_colormap = matplotlib.colors.LinearSegmentedColormap.from_list("temperature change", ["darkblue", "blue", "deepskyblue", "lightgrey", "lightgrey", "orangered", "red", "darkred"])
 
+    return temperature_change_colormap
+
+def dew_point_change_colormap():
+    dew_point_change_colormap = matplotlib.colors.LinearSegmentedColormap.from_list("dew point change", ["darkorange", "darkkhaki", "lightgrey", "forestgreen", "aqua"])
+
+    return dew_point_change_colormap
+
+def precipitation_colormap():
+    precipitation_colormap = matplotlib.colors.LinearSegmentedColormap.from_list("precipitation", ["magenta", "blue", "cyan", "lime", "darkgreen", "gold", "orange", "red", "darkred", "maroon", "dimgrey"])
+
+    return precipitation_colormap
+
+def precipitation_anomaly_colormap():
+    precipitation_anomaly_colormap = matplotlib.colors.LinearSegmentedColormap.from_list("precipitation anomaly", ["saddlebrown", "peru", "orange", "gold", "lightgrey", "lime", "darkgreen", "blue", "navy" ])
+
+    return precipitation_anomaly_colormap
+
+def relative_humidity_change_colormap():
+    relative_humidity_change_colormap = matplotlib.colors.LinearSegmentedColormap.from_list("relative humidity", ["saddlebrown", "peru", "orange", "gold", "lightgrey", "yellowgreen", "lawngreen", "springgreen", "lime"])
+    
+    return relative_humidity_change_colormap
+
+def vapor_pressure_deficit_colormap():
+    
+    vapor_pressure_deficit_colormap = matplotlib.colors.LinearSegmentedColormap.from_list("vpd", ["indigo", "purple", "magenta", "lightgrey", "gold", "darkorange", "saddlebrown"])
+    
+    return vapor_pressure_deficit_colormap
